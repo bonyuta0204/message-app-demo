96f54a7359437e0335834a04bc7305ec53964c81
Yuta Nakamura <yuta.nakamura@f-scratch.com>
2022-08-11 12:59:18 +0900

    Initial Commit

---
+++ MessageAppDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MessageAppDemo
+//
+//  Created by Yuta Nakamura on 2022/08/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
