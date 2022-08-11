5d93f7d03850c72f00b4d9811dd194114a902d85
Yuta Nakamura <yuta.nakamura@f-scratch.com>
2022-08-11 13:48:19 +0900

    feat: add ChatListViewController

--- MessageAppDemo/ViewController/ChatListViewController.swift
+++ MessageAppDemo/ViewController/ChatListViewController.swift
@@ -7,11 +7,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ChatListViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print(tableView)
     }
 
 
